3db78f0dcd17427c439bf7665b5bbb57892f56cc
Manolo Santos <manolo.santos@gmail.com>
2025-07-01 20:29:06 +0200

    Public exports

--- projects/ai-sdk-ng/src/public-api.ts
+++ projects/ai-sdk-ng/src/public-api.ts
@@ -2,4 +2,8 @@
  * Public API Surface of ai-sdk-ng
  */
 export { Chat } from "./lib/chat.ng";
-export { Completion } from "./lib/completion.ng";
+export { Completion, type CompletionOptions } from "./lib/completion.ng";
+export {
+  StructuredObject,
+  type StructuredObjectOptions,
+} from "./lib/structured-object.ng";
